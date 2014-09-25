b388c142815473d89548c757c766d25aa9b464d7
mbedoya <mauricio.bedoya@gmail.com>
2014-09-24 20:34:41 -0500

    First version - Plenum

---
+++ Chechere.WebSite/App_Start/FilterConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Chechere.WebSite
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+            filters.Add(new Plenum.WebSite.App_Start.Filters.FilterActions());
+        }
+    }
+}
